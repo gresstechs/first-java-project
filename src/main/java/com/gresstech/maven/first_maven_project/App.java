ac8ea91444da36310e2a9f4a80a48c4076239af8
chidera progress <gresstechsolutions@gmail.com>
2025-08-13 10:53:04 +0100

    First time push

---
+++ src/main/java/com/gresstech/maven/first_maven_project/App.java
@@ -0,0 +1,10 @@
+package com.gresstech.maven.first_maven_project;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
