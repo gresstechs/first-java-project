e0de0f133c9af0d99bf99b5ccbed29d79c41669c
Progress Nwaokwa <141236577+gresstechs@users.noreply.github.com>
2025-09-19 14:44:44 +0100

    Update output message in App.java

--- src/main/java/com/gresstech/maven/first_maven_project/App.java
+++ src/main/java/com/gresstech/maven/first_maven_project/App.java
@@ -5,6 +5,6 @@ package com.gresstech.maven.first_maven_project;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World! trenches");
     }
 }
